a02c524d96607306a30ccc9beb43515b709feed5
Hiroki Kondo <kondo.matr02@gmail.com>
2019-10-24 19:20:15 +0900

    printを追加　2回目

--- ProjectX/ViewController.swift
+++ ProjectX/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("天才集団(笑)")
+        print("あと、なんでGitHubのキャラクターは猫やねん？")
     }
 
 
